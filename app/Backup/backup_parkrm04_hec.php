211ce7d10f664e768e0b282dab37ade9c0e30f70
Parker Moore <parkrmoore@gmail.com>
2012-03-03 22:47:47 -0500

    Wrote backup script.

---
+++ app/Backup/backup_parkrm04_hec.php
@@ -0,0 +1,69 @@
+<?php
+
+/* backup the db OR just a table */
+function backup_tables($host, $user, $pass, $name, $tables = '*')
+{
+	
+	$link = mysql_connect($host,$user,$pass);
+	mysql_select_db($name,$link);
+	
+	//get all of the tables
+	if($tables == '*'){
+		$tables = array();
+		$result = mysql_query('SHOW TABLES');
+		while($row = mysql_fetch_row($result)){
+			$tables[] = $row[0];
+		}
+	}else{
+		$tables = is_array($tables) ? $tables : explode(',',$tables);
+	}
+	
+	//cycle through
+	foreach($tables as $table){
+		$result = mysql_query('SELECT * FROM '.$table);
+		$num_fields = mysql_num_fields($result);
+		
+		$return .= 'DROP TABLE '.$table.';';
+		$row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+		$return.= "\n\n".$row2[1].";\n\n";
+		
+		for ($i = 0; $i < $num_fields; $i++) {
+			while($row = mysql_fetch_row($result)){
+				$return.= 'INSERT INTO '.$table.' VALUES(';
+				for($j=0; $j<$num_fields; $j++) {
+					$row[$j] = addslashes($row[$j]);
+					$row[$j] = ereg_replace("\n","\\n",$row[$j]);
+					if (isset($row[$j])) { $return.= '"'.$row[$j].'"' ; } else { $return .= '""'; }
+					if ($j<($num_fields-1)) { $return.= ','; }
+				}
+				$return.= ");\n";
+			}
+		}
+		$return.="\n\n\n";
+	}
+	
+	mysql_close($link);
+	
+	//save file
+	$filename = dirname(__FILE__) . '/' . $name . '-backup-' . time() . '-' . (md5(implode(',',$tables))) . '.sql', 'w+';
+	return file_put_contents($return);
+}
+
+$tables = array(
+	'mobile_app_events',
+	'mobile_app_events_users',
+	'mobile_app_faqs',
+	'mobile_app_food_items',
+	'mobile_app_hashes',
+	'mobile_app_maps',
+	'mobile_app_menus',
+	'mobile_app_pages',
+	'mobile_app_speakers',
+	'mobile_app_sponsors',
+	'mobile_app_thoughts',
+	'mobile_app_users',
+);
+
+backup_tables('localhost', 'root', '', 'hec_mobile', $tables);	
+	
+?>
